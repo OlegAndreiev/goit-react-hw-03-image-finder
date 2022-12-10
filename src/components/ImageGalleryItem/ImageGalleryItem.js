60dc725755ae1da5581450ffb50e3c8fe7a6e981
Oleg Andreiev <mr.olegandreev@gmail.com>
2022-12-10 17:24:36 +0200

    Update ImageGalleryItem.js

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -5,6 +5,21 @@ import css from './ImageGalleryItem.module.css';
 class ImageGalleryItem extends React.Component {
   state = {
     searchedName: this.props.searchedName,
+    id: '',
+    webformatURL: '',
+    largeImageURL: '',
+    tags: '',
+    loading: false,
+  };
+
+  responceDataInput = responce => {
+    console.log(responce);
+    this.setState({
+      id: responce.hits[0].id,
+      webformatURL: responce.hits[0].webformatURL,
+      tags: responce.hits[0].tags,
+    });
+    console.log(this.state);
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -12,19 +27,28 @@ class ImageGalleryItem extends React.Component {
     const API_KEY = '30800366-aecfdce11bab1e79da5c222a8';
     const prevName = prevProps.searchedName;
     const newName = this.props.searchedName;
+
     if (prevName !== newName) {
+      this.setState({ loading: true });
       fetch(
         `${BASE_URL}?q=${newName}&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
       )
-        .then(responce => console.log(responce))
-        .then(console.log);
+        .then(responce => responce.json())
+        .then(data => this.responceDataInput(data))
+        .finally(() => this.state({ loading: false }));
+      // .then(console.log);
     }
   }
 
   render() {
     return (
       <li className={css.ImageGalleryItem}>
-        <img className={css.ImageGalleryItemImage} src="" alt="" />
+        {this.state.loading && <div>Зугружаем</div>}
+        <img
+          className={css.ImageGalleryItemImage}
+          src={this.state.webformatURL}
+          alt={this.state.tags}
+        />
       </li>
     );
   }
